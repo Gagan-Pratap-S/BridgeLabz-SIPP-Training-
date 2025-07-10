ae18090189123c115d67083004df47e45ea384c9
Gagan-Pratap-S <154469460+Gagan-Pratap-S@users.noreply.github.com>
2025-07-10 12:34:54 +0530

    factor

--- Day4_Methods/_MatrixManipulation.java
+++ Day4_Methods/_MatrixManipulation.java
@@ -59,7 +59,7 @@ public class _MatrixManipulation {
         inv[1][2] = (m[0][2]*m[1][0] - m[0][0]*m[1][2]) / (double) det;
 
         inv[2][0] = (m[1][0]*m[2][1] - m[1][1]*m[2][0]) / (double) det;
-        inv[2][1] = (m[0][1]*m[2][0] - m[0][0]*m[2][1]) / (double) det;
+        inv[2][1] = (m[0][1]*m[2][0] - m[0][0]*m[2][1]) / (double) det;c:\Users\Lenovo\Downloads\Day4_Methods\Day4_Methods\FactorAnalysis.java
         inv[2][2] = (m[0][0]*m[1][1] - m[0][1]*m[1][0]) / (double) det;
         return inv;
     }
