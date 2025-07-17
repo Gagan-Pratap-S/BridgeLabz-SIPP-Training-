39f145ed90528708bcdb072c2c9dd3fa5b78fb27
Gagan-Pratap-S <154469460+Gagan-Pratap-S@users.noreply.github.com>
2025-07-17 10:54:29 +0530

    Add linked list program

---
+++ Workshop/LinkedList/Main.java
@@ -0,0 +1,25 @@
+package LinkedList;
+public class Main {
+    public static void main(String [] args){
+        Linked list = new Linked();
+        list.add(20);
+        // l.add(30);
+
+        // l.append(35);
+        // l.append(40);
+
+        // System.out.println("Adding and appending done");
+
+        list.printList();
+
+
+        // l.delete(20);
+        // l.printList();
+
+        // System.out.println("20 is deleted");
+
+
+        
+      //  System.out.println( "Searching 35 : " + l.search(35));
+    }
+}
